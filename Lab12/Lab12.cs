e3514f19bd4eaee1b0c21b245d80e96daa4ef535
chris1volfan <chris1volfan@gmail.com>
2022-11-21 20:10:27 -0500

    Labs including Lab12 are completed

--- Lab12/Lab12.cs
+++ Lab12/Lab12.cs
@@ -21,14 +21,15 @@ namespace Lab12
 
         
         {
-            //declare arrays
+            //declare arrays, including the amount of dice rolls
             uint[] d4Rolls = new uint[4];
             uint[] d6Rolls = new uint[6];
             uint[] d8Rolls = new uint[8];
             uint[] d10Rolls = new uint[10];
             uint[] d12Rolls = new uint[12];
             uint[] d20Rolls = new uint[20];
-
+            
+            //call RollDice method
             RollDice(d4Rolls, 4);
             RollDice(d6Rolls, 6);
             RollDice(d8Rolls, 8);
@@ -36,19 +37,23 @@ namespace Lab12
             RollDice(d12Rolls, 12); 
             RollDice(d20Rolls, 20); 
 
+            //Display results of random dice rolls
             Console.Write("d4 rolls: " + ArrayToString(d4Rolls) + "\n");
-            //RollDice(d4Rolls, 4);
-            //create a object of type Random
-            Random r = new Random();
+            Console.Write("d6 rolls: " + ArrayToString(d6Rolls) + "\n");
+            Console.Write("d8 rolls: " + ArrayToString(d8Rolls) + "\n");
+            Console.Write("d10 rolls: " + ArrayToString(d10Rolls) + "\n");
+            Console.Write("d12 rolls: " + ArrayToString(d12Rolls) + "\n");
+            Console.Write("d20 rolls: " + ArrayToString(d20Rolls) + "\n");
             
-            {
-
-            //    Console.Write(" " + RollDice(1));
-            }
-           
+            //allow display to remain until user presses a key
             {
                 Console.ReadKey();
             }
+
+            //create a object of type Random
+            Random r = new Random();
+                              
+            
         }
         
         //Method for getting random dice rolls with various number of sides 
@@ -56,82 +61,38 @@ namespace Lab12
         static uint RollDice(uint numSides)
         {
             Random r = new Random();
-            int temp = r.Next(1, Convert.ToInt32(numSides));
+            int temp = r.Next(1, Convert.ToInt32(numSides +1));
             uint output = Convert.ToUInt32(temp);
             return output;
         }
+        //A method for every element in diceRolls is set to a random value rolled by using
+        //the method RollDice(unit sides)
         static void RollDice(uint[] diceRolls, uint sides)
         {
-            //sets up an array for each dice array whin called
-            uint[] diceArray = new uint[diceRolls.Length];
-            string result;
+            //sets up an array for each dice array when called
+            //uint[] diceArray = new uint[diceRolls.Length];
+            //string result;
 
             for (int i = 0; i < diceRolls.Length; i++)
             {
-                diceArray[i] = RollDice(sides);
+                diceRolls[i] = RollDice(sides);
             }
-            result = ArrayToString(diceArray);
+            //result = diceArray;
         }
+        // A method for every element in array, to concatenate the element's value and add a space to a string between
+        // variable upon return of the string variable
         static string ArrayToString(uint[] array)
         {
-            //string rolls;
+            //string output;
             //add value for loop display
-            string diceRolls = "";
-            foreach(uint roll in array)
+            string output = "";
+            foreach(uint item in array)
             {
-                diceRolls += roll + " ";
+            //rolls = Convert.ToString(roll);
+                output += item + " ";
             }   
-            return diceRolls;
+            return output;
         }
-        //intiate six sided random dice roll            
-        //
-        //{
-        //    Console.Write("d4 = ");
-        //}
-        //for (int i = 0; i < 4; i++)
-        //{
-        //
-        //    Console.Write(" " + RollDice(5));
-        //}
-        //{
-        //    Console.Write("\nd6 = ");
-        //}
-        //for (int i = 0; i< 6; i++)
-        //{
-        //     
-        //     Console.Write(" "+ RollDice(7));
-        // }
-        //intiate eight sided random dice roll 
-        //{
-        //    Console.Write("\nd8 = ");
-        // }
-        // for (int i = 0; i < 8; i++)
-        //{
-        //     
-        //    Console.Write(" " + RollDice(9));
-        // }
-        //intiate ten sided random dice roll 
-        //{
-        //     Console.Write("\nd10 = ");
-        // }
-        // for (int i = 0; i < 10; i++)
-        // {
-        //
-        //    Console.Write(" " + RollDice(11));
-        //}
-        //intiate twelve sided random dice roll 
-        //{
-        //     Console.Write("\nd12 = ");
-        //}
-        // for (int i = 0; i < 12; i++)
-        //intiate twenty sided random dice roll 
-        //{
-        //    Console.Write("\nd20 = ");
-        //}
-        //for (int i = 0; i < 20; i++)
-        //{
-        //
-        //    Console.Write(" " + RollDice(20));
-        //}
+       
     }
 }
