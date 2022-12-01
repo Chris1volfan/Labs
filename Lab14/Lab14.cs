5d0b2082ad05aa404740c4c5874818342a20124f
chris1volfan <chris1volfan@gmail.com>
2022-11-30 21:07:05 -0500

    finished Lab14

---
+++ Lab14/Lab14.cs
@@ -0,0 +1,56 @@
+﻿/*///<summary>
+ ////File: Lab 14
+ ///Name: Chris Brown
+ ////Email: Cbrown36@stumail.northeaststate.edu
+ ///Class: CISP1010
+ ///Semester: Fall 2022
+ ///Lab 14: Object Modeling - Regular Polygon
+ /// </summary>
+ */
+
+namespace Lab14
+{
+    /// <summary>
+    /// A program that intiates using a regular polygon object three regular poygons with varying number 
+    /// of sides and lengths. All three polygons are then displayed to show the number of sides the lenght
+    /// and the calculated perimeter, and area.
+    /// </summary>
+    internal class Lab14
+    {
+        /// <summary>
+        /// The Main method is the entry point for this program
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // establish the regular poygon objects
+            RegularPolygon regularPolygon1 = new RegularPolygon();
+            RegularPolygon regularPolygon2 = new RegularPolygon(6, 4.0);
+            RegularPolygon regularPolygon3 = new RegularPolygon();
+
+            // set the property values for Polygon 3
+            regularPolygon3.NumSides = 12;
+            regularPolygon3.SideLength = 1.25;
+
+            // Display the polygons to include values and calculated perimeter and area for each
+            Console.WriteLine($"regularPolygon1");
+            Console.WriteLine($"\tnumSides = {regularPolygon1.NumSides}");
+            Console.WriteLine($"\tsideLength = {regularPolygon1.SideLength}");
+            Console.WriteLine($"\tperimeter = {regularPolygon1.Perimeter}");
+            Console.WriteLine($"\tarea = {regularPolygon1.Area}");
+            Console.WriteLine();
+            Console.WriteLine($"regularPolygon2");
+            Console.WriteLine($"\tnumSides = {regularPolygon2.NumSides}");
+            Console.WriteLine($"\tsideLength = {regularPolygon2.SideLength}");
+            Console.WriteLine($"\tperimeter = {regularPolygon2.Perimeter}");
+            Console.WriteLine($"\tarea = {regularPolygon2.Area}");
+            Console.WriteLine();
+            Console.WriteLine($"regularPolygon3");
+            Console.WriteLine($"\tnumSides = {regularPolygon3.NumSides}");
+            Console.WriteLine($"\tsideLength = {regularPolygon3.SideLength}");
+            Console.WriteLine($"\tperimeter = {regularPolygon3.Perimeter}");
+            Console.WriteLine($"\tarea = {regularPolygon3.Area}");
+            /// Pause to display results and user to press key
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab14/RegularPolygon.cs
@@ -0,0 +1,94 @@
+﻿
+/*///<summary>
+ ////File: RegularPolygon
+ ///Name: Chris Brown
+ ////Email: Cbrown36@stumail.northeaststate.edu
+ ///Class: CISP1010
+ ///Semester: Fall 2022
+ ///Lab 14: Object Modeling - RegularPolygon
+ /// </summary>
+ */
+
+namespace Lab14
+{
+    internal class RegularPolygon
+    {
+        /// <summary>
+        /// Declare variables
+        /// </summary>
+        private uint numSides;
+        private double sideLength;
+        /// <summary>
+        /// intiate the default contructor and set default property values
+        /// </summary>
+        public RegularPolygon()
+        {
+            numSides = 3;
+            sideLength = 1;
+        }
+        /// <summary>
+        /// Sets up the RegularPoygon object by with establishing parameters of number of sides and the
+        /// length of the sides
+        /// </summary>
+        /// <param name="numSides"></param>
+        /// <param name="sideLength"></param>
+        public RegularPolygon(uint numSides, double sideLength)
+        {
+            this.numSides = numSides;
+            this.sideLength = sideLength;
+        }
+        /// <summary>
+        /// Gets and sets numSides for valid values which are greater than or equal to 3
+        /// </summary>
+        public uint NumSides
+        {
+            set
+            {
+                if (numSides >= 3)
+                {
+                    numSides = value;
+                }
+            }
+            get { return numSides; }
+        }
+        /// <summary>
+        /// Gets and sets sideLength for valid values which are greater than or equal to 1
+        /// </summary>
+        public double SideLength
+        {
+            set
+            {
+                if (sideLength >= 1)
+                {
+                    sideLength = value;
+                }
+            }
+            get { return sideLength; }
+        }
+        /// <summary>
+        /// The Area of the Polygon is calculated by using the Polygon's number of sides and lenth using
+        /// a formula
+        /// </summary>
+        public double Area
+        {
+
+            get
+            {
+                return numSides * Math.Pow(sideLength, 2) / (4 * (Math.Tan(Math.PI / numSides)));
+            }
+
+        }
+        /// <summary>
+        /// The Perimeter of the Polygon
+        /// </summary>
+        public double Perimeter
+        {
+
+            get
+            {
+                return numSides * sideLength;
+            }
+
+        }
+    }
+}
